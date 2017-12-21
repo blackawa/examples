6ec1176e160beab08de1b70bbc9b823f6a292741
blackawa <3104kurokawa@gmail.com>
2017-12-21 17:02:29 +0900

    :sparkles:

--- gs-integrant/src/gs_integrant/core.clj
+++ gs-integrant/src/gs_integrant/core.clj
@@ -1,7 +1,26 @@
 (ns gs-integrant.core
-  (:gen-class))
+  (:gen-class)
+  (:require [integrant.core :as ig]
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :as resp]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def config
+  {:adapter/jetty {:port 8080 :handler (ig/ref :handler/greet)}
+   :handler/greet {:name "alice"}})
+
+(defmethod ig/init-key :adapter/jetty [_ {:keys [handler] :as opts}]
+  (let [server (jetty/run-jetty handler (-> opts (dissoc :handler) (assoc :join? false)))]
+    (println server)
+    server))
+
+(defmethod ig/halt-key! :adapter/jetty [_ server]
+  (println server)
+  (.stop server))
+
+(defmethod ig/init-key :handler/greet [_ {:keys [name]}]
+  (fn [_] (resp/response (str "Hello " name))))
