83297a81f0d3d1819863483dbc831f79cb5f7c83
blackawa <3104kurokawa@gmail.com>
2018-03-26 15:30:17 +0900

    :tada: block rum/mount until state is ready

--- gsfigwheel/src/client.cljs
+++ gsfigwheel/src/client.cljs
@@ -1,11 +1,9 @@
 (ns gsfigwheel.client
   (:require [accountant.core :as accountant]
             [bidi.bidi :as bidi]
-            [cljs.core.async :refer [<! timeout]]
             [rum.core :as rum]
             [gsfigwheel.flow :as flow]
-            [gsfigwheel.view :as view])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [gsfigwheel.view :as view]))
 
 (enable-console-print!)
 
@@ -18,7 +16,10 @@
                                                 (:handler (bidi/match-route view/routes path)))))
     :path-exists? (fn [path]
                     (boolean (bidi/match-route view/routes path)))})
-  (go
-    (accountant/dispatch-current!)
-    (<! (timeout 1000))
-    (rum/mount (view/+app flow/state) app)))
+  (accountant/dispatch-current!)
+  (add-watch flow/state :handler-readiness
+             (fn [k a old new]
+               (println "catch watch")
+               (when (boolean (:handler new))
+                 (remove-watch flow/state :handler-readiness)
+                 (rum/mount (view/+app flow/state) app)))))
