8df983e2866175d97896860ee47033a3e1e1182d
blackawa <3104kurokawa@gmail.com>
2018-05-10 19:01:57 +0900

    :sparkles: build scss with webpack

--- breakout-playground/src/js/component/atom/Block.js
+++ breakout-playground/src/js/component/atom/Block.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Block = (prop) => {
+const Block = () => {
   return (<span className={'block'}>block</span>);
 };
 
--- breakout-playground/webpack.config.js
+++ breakout-playground/webpack.config.js
@@ -1,25 +1,44 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: ['./src/js/index'],
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    devtool: false,
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
+  mode: 'production',
+  entry: ['./src/js/index', './src/scss/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              file: 'src/scss/main.scss',
+              includePaths: ['src/scss']
             }
+          }
         ]
-    }
+      }
+    ]
+  }
 };
