7d1216a79ec5faf78fb93909b4b8064ab6dc1b43
blackawa <3104kurokawa@gmail.com>
2018-05-12 17:56:54 +0900

    :sparkles: react with style

--- gs-storybook-react-and-css/src/js/index.js
+++ gs-storybook-react-and-css/src/js/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
+import '../css/main.css';
 
 const app = document.getElementById('app');
 ReactDOM.render(<App>Hello from react!</App>, app);
--- gs-storybook-react-and-css/webpack.config.js
+++ gs-storybook-react-and-css/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
@@ -21,6 +21,10 @@ module.exports = {
                         ]
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             }
         ]
     }
