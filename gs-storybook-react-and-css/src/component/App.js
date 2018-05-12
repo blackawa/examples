f152049bee98bd9982d9e8725a61ff017e13fdda
blackawa <3104kurokawa@gmail.com>
2018-05-12 17:38:33 +0900

    :seedling:

---
+++ gs-storybook-react-and-css/src/component/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = (prop) => (<div className="block">{prop.children}</div>);
+
+export default App;
---
+++ gs-storybook-react-and-css/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App';
+
+const app = document.getElementById('app');
+ReactDOM.render(<App>Hello from react!</App>, app);
