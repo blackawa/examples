366e751ba3235e4345115b3588f07465a6d4cf4e
blackawa <3104kurokawa@gmail.com>
2018-05-17 15:11:45 +0900

    :sparkles: raw react and local state

--- getting-started-of-redux/src/component/App.js
+++ getting-started-of-redux/src/component/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const App = () => {
-  return (<div>Hello~</div>);
-};
+import TextField from './TextField';
+import PreviewField from './PreviewField';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'hello'
+    };
+  }
+
+  onTextChange(text) {
+    this.setState({text: text})
+  }
+
+  render() {
+    return (
+        <div>
+          <TextField value={this.state.text} onChange={this.onTextChange.bind(this)}/>
+          <PreviewField text={this.state.text}/>
+        </div>
+    )
+  }
+}
 
 export default App;
