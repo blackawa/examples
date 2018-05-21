9cb9a4f39fb11f674364376a31cb1d5439c09954
blackawa <3104kurokawa@gmail.com>
2018-05-21 14:13:15 +0900

    :seedling: simple react tabs page

---
+++ gs-react-router/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const Page1 = () => <div>
+  <div className='tabs'>
+    <ul>
+      <li className='is-active'><a>Page 1</a></li>
+      <li><a>Page 2</a></li>
+    </ul>
+  </div>
+  <div className='content'>This is Page1.</div>
+</div>;
+
+const Page2 = () => <div>
+  <div className='tabs'>
+    <ul>
+      <li><a>Page 1</a></li>
+      <li className='is-active'><a>Page 2</a></li>
+    </ul>
+  </div>
+  <div className='content'>This is Page2.</div>
+</div>;
+
+const App = () => <section className='section'>
+  <div className='container'>
+    <Page1/>
+  </div>
+</section>;
+
+render(<App/>, document.getElementById('app'));
