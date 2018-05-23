9100d02e4cb93fa644013433537d1bcffd0d480d
blackawa <3104kurokawa@gmail.com>
2018-05-23 17:05:57 +0900

    :tada: use react router

--- react-router/src/components/App.js
+++ react-router/src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
-const App = () => <div>Hello from react. env is {process.env.NODE_ENV}</div>;
-const WrappedApp = process.env.NODE_ENV === 'development' ? hot(module)(App) : App
+const Home = () => <div><h2>This is home.</h2></div>;
+const About = () => <div><h2>This is about.</h2></div>;
+
+const App = () => <BrowserRouter>
+  <div>
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+    </ul>
+    Hello from react. env is {process.env.NODE_ENV}
+    <Route exact path='/' component={Home}/>
+    <Route path='/about' component={About}/>
+  </div>
+</BrowserRouter>;
+
+const WrappedApp = process.env.NODE_ENV === 'development' ? hot(module)(App) : App;
 
 export default WrappedApp;
