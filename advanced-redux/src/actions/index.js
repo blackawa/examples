3c35d0464972fa1af932f53d31e77b175c17c9ce
blackawa <3104kurokawa@gmail.com>
2018-05-28 17:20:52 +0900

    :sparkles: create network request action/reducer

--- advanced-redux/src/actions/index.js
+++ advanced-redux/src/actions/index.js
@@ -2,3 +2,13 @@ export const CHANGE_QUERY = 'CHANGE_QUERY';
 export const changeQuery = (query) => {
   return {type: CHANGE_QUERY, query: query}
 };
+
+export const SUBMIT_QUERY = 'SUBMIT_QUERY';
+export const submitQuery = (query) => {
+  return {type: SUBMIT_QUERY, query: query}
+};
+
+export const RECEIVE_REPOSITORIES = 'RECEIVE_REPOSITORIES';
+export const receiveRepositories = (repositories) => {
+  return {type: RECEIVE_REPOSITORIES, repositories: repositories}
+};
--- advanced-redux/src/reducers/index.js
+++ advanced-redux/src/reducers/index.js
@@ -1,9 +1,11 @@
-import {CHANGE_QUERY} from "../actions";
+import {CHANGE_QUERY, SUBMIT_QUERY} from "../actions";
 
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case CHANGE_QUERY:
       return Object.assign({}, state, {query: action.query});
+    case SUBMIT_QUERY:
+      return Object.assign({}, state, {processing: true});
     default:
       return state
   }
