423653fed9f887302b0e4ebbe13421ee9741a6ae
blackawa <3104kurokawa@gmail.com>
2018-08-17 19:28:02 +0900

    :sparkles: query to rdb

--- monolith/src/clj/monolith/handler/api/dog.clj
+++ monolith/src/clj/monolith/handler/api/dog.clj
@@ -1,5 +1,6 @@
 (ns monolith.handler.api.dog
-  (:require [integrant.core :as ig]))
+  (:require [integrant.core :as ig]
+            [monolith.boundary.dog :as dog]))
 
-(defmethod ig/init-key ::index [_ _]
-  (fn [req] {:status 200 :body {:dogs [{:name "Gon"} {:name "Sasuke"}]}}))
+(defmethod ig/init-key ::index [_ {:keys [db]}]
+  (fn [req] {:status 200 :body {:dogs (dog/fetch-all db)}}))
